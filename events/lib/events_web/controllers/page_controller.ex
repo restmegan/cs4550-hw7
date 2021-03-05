8b79d1b9ecf60120f087c45d0ffc43084bcdd3b6
Megan Rest <megan@guest.guest>
2021-03-05 23:42:35 +0000

    events in index

--- events/lib/events_web/controllers/page_controller.ex
+++ events/lib/events_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule EventsWeb.PageController do
   use EventsWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    eventposts = EventPosts.list_eventposts()
+    render(conn, "index.html", eventposts: eventposts)
   end
 end
